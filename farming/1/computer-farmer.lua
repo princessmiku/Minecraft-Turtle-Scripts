e1866054b7fc75cde9274c12b346f213a60f1bf1
Miku <41338209+princessmiku@users.noreply.github.com>
2020-08-17 15:30:19 +0200

    Create computer-farmer.lua

---
+++ farming/1/computer-farmer.lua
@@ -0,0 +1,14 @@
+-- not finish
+-- Field
+ 
+height = 5
+widht = 5
+ 
+turtle_id = 28
+ 
+rednet.open("right")
+rednet.send(turtle_id, ""..height..","..widht)
+ 
+function updateDisplay (turtle_pos)
+ 
+end
